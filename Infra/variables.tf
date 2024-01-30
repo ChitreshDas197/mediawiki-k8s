e132aa61700dfcc89498215bc27107953b9eaf6f
ChitreshDas197 <chitreshdas305@gmail.com>
2024-01-30 17:37:20 +0530

    Configured credentials on environment variable and removed from terraform.tfvars

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,19 +1,3 @@
-# Below variables are used for authenticating terrfarm with Azure via Service Principle and Client Secret
-# Input values from environment variables (recommended) / *tfvars file (please add *tfvars in .gitignore while checking in the code to VCS)
-
-variable "client_id" {
-
-}
-variable "client_secret" {
-
-}
-variable "subscription_id" {
-
-}
-variable "tenant_id" {
-
-}
-
 
 #Below variables are used for resource group
 # I have used east us 2 as azure region - give your preferred region as default or via *.tfvars file as applicable
