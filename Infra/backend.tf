98fea0462350c651a75daa3e90cb8e9ae40930ce
ChitreshDas197 <chitreshdas305@gmail.com>
2024-01-30 16:01:24 +0530

    update backend

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "mediawiki-devops-rg"
-    storage_account_name = "terrastatefileaccount"
+    resource_group_name  = "storage"
+    storage_account_name = "tfstatestoreacc01"
     container_name       = "tfstateblob"
     key                  = "mediawiki.terraform.tfstate"
   }
