bbd523e32121b380ec2af40ae00253ef167b216c
ChitreshDas197 <chitreshdas305@gmail.com>
2024-02-02 22:08:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,9 @@ resource "azurerm_kubernetes_cluster" "mediawiki-k8s-cluster" {
   location            = azurerm_resource_group.mediawiki_rg.location
   resource_group_name = azurerm_resource_group.mediawiki_rg.name
   dns_prefix          = "mediawiki-dns"
-
+    depends_on = [
+    azurerm_resource_group.mediawiki_rg
+  ]
   default_node_pool {
     name       = "default"
     node_count = var.NODE_COUNT
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 # Used variables from env which are set using TF_VAR_ prefix
 # Uppercase is used as declaring env vars using TF_VAR_ makes them upper case
 # Here the env variables TF_VAR_ is defined on pipeine itself as pipeline variables
+# You can also initialize these variables using terraform.tfvars file using the below variable names in a key value pair
 
 
 variable "RG_NAME" {
