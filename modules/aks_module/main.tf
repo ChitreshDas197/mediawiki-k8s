dc1b20879de6bd29b62babf97ac54a69de81fc0e
ChitreshDas197 <chitreshdas305@gmail.com>
2024-02-29 02:56:48 +0530

    Add remote backend and change naming convenstions of k8s block

--- modules/aks_module/main.tf
+++ modules/aks_module/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_kubernetes_cluster" "mediawiki-k8s-cluster" {
-  name                = "${var.location}-mediawiki-cluster"
+  name                = "${var.rg_name}-mediawiki-cluster"
   location            = var.location
   resource_group_name = var.rg_name
-  dns_prefix          = "${var.location}-mwiki-k8s"
+  dns_prefix          = "${var.rg_name}-mwiki-k8s"
   
   default_node_pool {
-    name       = "${var.location}-k8s-node-pool"
+    name       = "${var.location}pool"
     node_count = var.node_count
     vm_size    = var.vm_size
   }
